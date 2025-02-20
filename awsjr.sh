969e49c109b2dc7d630d3c0ffa1c4850cc759c19
Santosjr10 <alexandresantosjr32@gmail.com>
2025-02-20 18:54:16 -0300

    Update awsjr.sh

--- awsjr.sh
+++ awsjr.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-logs="var/log/user_data.log"
-echo"Executando o Script" > $logs
+logs="/var/log/user_data.log"
+echo "Executando o Script" > $logs
 
-echo"atualizando pacote" | tee -a $logs
-sudo apt upadate -y >> $logs
+echo "atualizando pacote" | tee -a $logs
+sudo apt update -y >> $logs
 
-echo"instalando o apache" | tee -a $logs
+echo "instalando o apache" | tee -a $logs
 
 sudo apt install -y apache2  >> $logs
 sudo systemctl start apache2 >> $logs
 sudo systemctl enable apache2 >> $logs
 
-echo"instalando o git" | tee -a $logs
+echo "instalando o git" | tee -a $logs
 sudo apt install -y git >> $logs
 
-echo"clonando repositório com site" | tee -a $logs
+echo "clonando repositório com site" | tee -a $logs
 sudo git clone https://github.com/Santosjr10/ScriptAWS.git /tmp/atividade >> $logs
 sudo mv /tmp/atividade/ScriptAWS/index.html /var/www/html/ >> $logs
 
-echo"ajustando permissões" | tee -a $logs
+echo "ajustando permissões" | tee -a $logs
 sudo chmod -R 755 /var/www/html/ >> $logs
 
 
-echo"Reiniciando o apache" | tee -a $logs
+echo "Reiniciando o apache" | tee -a $logs
 sudo systemctl restart apache2 >> $logs 
 sudo systemctl status apache2 >> $logs
 
-echo" Enviando uma solicitação post" | tee -a $logs
+echo "Enviando uma solicitação post" | tee -a $logs
 curl -X POST -d "nome=Alexandre Junior" https://difusaotech.com.br/lab/aws/index.php 
 
-echo"finalizando o Script" | tee -a $logs
+echo "finalizando o Script" | tee -a $logs
 
 
